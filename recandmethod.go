43ff0f91c6f51d3ba0520f270c1337a9070de4c5
circusex <discocircus001@gmail.com>
2017-01-31 21:52:34 -0800

    Create recandmethod.go

---
+++ recandmethod.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	last string
+	age  int
+	walk string
+}
+
+func main() {
+	p1 := person{"Moneypenny", 24, "is walking"}
+	fmt.Println(p1)
+	p1.foo()
+}
+
+func (p person) foo() {
+	fmt.Println("Hello from", p.last)
+	fmt.Println(p.last, p.walk)
+}
+
+// func (receiver) identifier(parameters) (returns) {}
