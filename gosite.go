f76cdd5ec532f4e7864c29f327d489b00be36baa
circusex <discocircus001@gmail.com>
2017-02-09 12:34:46 -0800

    Create gosite.go

---
+++ gosite.go
@@ -0,0 +1,29 @@
+/*day 1 of week 5 assignment
+* Thursday 09.02.2017
+* began work 12:23
+*/
+
+package main
+
+import(
+"net/http"
+"html/template"
+)
+
+var tpl template.Template
+/*there's a templates repository I made for all the .gohtml files for the assignment(s)-
+https://github.com/circusex/server-side-scripting/tree/templates
+*/
+
+func init{
+tpl = template.Must(template.ParseGlob("*.gohtml"))
+}
+
+func main(){
+http.("/", homepage)
+http.ListenAndServe(":8080", nil)
+}
+
+func homepage(w http.ResponseWriter, r *httpRequest){
+tpl.ExecuteTemplate(w, `home.gohtml`, nil)
+}
