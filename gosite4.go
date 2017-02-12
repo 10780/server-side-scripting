dc3118c86dd59e0f546f50483a87357be278abdb
circusex <discocircus001@gmail.com>
2017-02-12 01:12:31 -0800

    Create gosite4.go

---
+++ gosite4.go
@@ -0,0 +1,26 @@
+/*day 4 of week 5 assignment
+* 12.02.2107
+* began work 1:09
+*/
+
+package main
+
+import(
+"net/http"
+"html/template"
+)
+
+var tpl template.Template
+
+func init{
+tpl = template.Must(template.ParseGlob("*.gohtml"))
+}
+
+func main(){
+http.("/", homepage)
+http.ListenAndServe(":8080", nil)
+}
+
+func homepage(w http.ResponseWriter, r *http.Request){
+tpl.ExecuteTemplate(w, `home.gohtml`, nil)
+}
