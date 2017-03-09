7b1e64b6ba90bb3aca479b4aa77f82fa6c5e52c1
circusex <discocircus001@gmail.com>
2017-03-08 16:37:12 -0800

    Add files via upload

---
+++ week8_6.go
@@ -0,0 +1,26 @@
+/*day 7 of week 8 assignment
+*work started 4:25 03.08.2017
+*/
+
+package main
+
+import(
+"net/http"
+"html/template"
+)
+
+var tpl *template.Template
+
+func init)(){
+tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main(){
+http.HandleFunc("/", index)
+http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("/assets")))
+http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+http.ExecuteTemplate(w, "index6.gohtml")
+}
