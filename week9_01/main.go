e674c793ed260182fcd27cb4eaedb88108b1d00f
circusex <discocircus001@gmail.com>
2017-03-28 13:04:20 -0700

    Add files via upload

---
+++ week9_01/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+"net/http"
+"fmt"
+)
+
+func main(){
+//http.HandleFunc("/", set)
+http.HandleFunc("/dog/bowzer", dog_bowzer)
+http.HandleFunc("/cat", cat)
+http.ListenAndServe(":8080", nil)
+}
+
+func dog_bowzer(w http.ResponseWriter, r *http.Request){
+http.SetCookie(w, &http.Cookie {
+Name: "site-cookie",
+Value: "value", 
+})
+fmt.Fprintln(w, "COOKIE SET")
+}
+
+func cat(w http.ResponseWriter, r *http.Request){
+c, err := r.Cookie("site-cookie")
+if err != nil {
+http.Error(w, http.StatusText(400), http.StatusBadRequest)
+return
+}
+fmt.Fprintln(w, "COOKIE: ", c)
+}
