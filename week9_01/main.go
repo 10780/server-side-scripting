776ff12db34eaea269ba319b1bd50542c4b625f2
circusex <discocircus001@gmail.com>
2017-04-02 16:21:17 -0700

    Update main.go

--- week9_01/main.go
+++ week9_01/main.go
@@ -9,7 +9,7 @@ import (
 var tpl template.Template
 
 func init(){
-tpl = template.Must(template.ParseGlob("*.gohtml"))
+tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 func main(){
